229f55a01712265fd9b7b4466450ef7922d9aa35
danielbayta <dennisdanielbayta@gmail.com>
2022-03-03 11:21:44 +0800

    Revert "created & migrated Customer and Account Models, registered in admin.py"
    This reverts commit 3a353c88897df7fd57cbdedb5fb09e13cac8da55.

--- customers_DB/customersapp/admin.py
+++ customers_DB/customersapp/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Customer
-from .models import Account
 
 # Register your models here.
-
-admin.site.register(Customer)
-admin.site.register(Account)
