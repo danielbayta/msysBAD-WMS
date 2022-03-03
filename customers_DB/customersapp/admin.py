3a353c88897df7fd57cbdedb5fb09e13cac8da55
danielbayta <dennisdanielbayta@gmail.com>
2022-03-03 11:05:46 +0800

    created & migrated Customer and Account Models, registered in admin.py

--- customers_DB/customersapp/admin.py
+++ customers_DB/customersapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer
+from .models import Account
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Account)
--- customers_DB/customersapp/models.py
+++ customers_DB/customersapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    Name = models.CharField(max_length = 300)
+    address = models.CharField(max_length = 300)
+    contact_number = models.CharField(max_length=12)
+    emailaddress = models.EmailField(max_length = 300)
+    
+    def getName(self):
+        return self.Name
+    
+    def __str__(self):
+        return str(self.pk) + ": " + str(self.Name) + "," + str(self.address) + ", " + str(self.contact_number) + ", " + str(self.emailaddress) 
+
+
+class Account(models.Model):
+    username = models.CharField(max_length=250, unique = True)
+    password = models.CharField(max_length=250)
+    # objects = models.Manager()
+
+    def getUsername(self):
+        return f"{self.username}"
+
+    def getPassword(self):
+        return f"{self.password}"
+
+    def __str__(self):
+        return f"{self.pk}: {self.username}"
